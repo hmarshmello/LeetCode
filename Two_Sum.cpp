a505b5c9f7caa3d37c7d54fee701840ccbf6bb29
Harsh Shrivastava <62792682+hmarshmello@users.noreply.github.com>
2022-09-16 11:22:59 +0530

    LeetCode_Easy

--- Two_Sum.cpp
+++ Two_Sum.cpp
@@ -4,51 +4,30 @@
 using namespace std;
 
 
-int m(){
-    int nums; 
-    cout << "No. Of Elements in the Array(>=2): ";
-    cin >> nums;
-    int a[nums];
-    
-    cout << "Elements of the Array: ";
-    for(int i = 0; i<nums;i++) cin >> a[i];
-    
-    cout << "Target Sum: ";
-    int target; cin >> target;
-    
-    vector<int> result;
-    for(int i = 0;i<nums;i++){
-        for(int j = i+1;j<nums;j++){
-            if(a[i] + a[j] == target){result.push_back(i); result.push_back(j);}
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
+        for(int i = 0;i<nums.size();i++){
+            for(int j = i+1;j<nums.size();j++){
+                if(nums[i] + nums[j] == target){result.push_back(i); result.push_back(j);}
+            }
         }
+        //cout << result[0] << " " << result[1];
+        return result;
     }
-    for(int i=0; i<result.size(); i++) cout << result.at(i) << ' ';
-}
+};
+
 
 int main(){
-    /*int t = 5;
-    rep:
-        int n; 
-        cout << "No. Of Elements in the Array(>=2): ";
-        cin >> n;
-        int a[n];
-        cout << "Elements of the Array: ";
-        for(int i = 0; i<n;i++) cin >> a[i];
-        cout << "Target Sum: ";
-        int target,flag = 0; cin >> target;
+    int n, num; cin >> n;
+    vector <int> nums;
+    for(int i = 0; i<n;i++){cin >> num; nums.push_back(num);}
 
-        for(int i = 0;i<n;i++){
-            for(int j = i+1;j<n;j++){
-                if(a[i] + a[j] == target) {
-                    cout << "Found the match at: [" << i << "," << j << "]" << endl;
-                    flag = 1;
-                }
-            }
-                if(flag != 1) {cout << "Target is Invalid\n"; break;}
-        }
-    while(t--) {cout << "Rem: " << t << endl; goto rep;}*/
-    m();
-    return 0;
+    int target; cin >> target;
+
+    Solution S1;
+    S1.twoSum(nums,num);
 }
 
 
