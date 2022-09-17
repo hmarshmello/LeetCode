56d7e97b9411646a0204493c288438060526b663
Harsh Shrivastava <62792682+hmarshmello@users.noreply.github.com>
2022-09-18 02:17:28 +0530

    LeetCode

---
+++ PalindromeNumber.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int x,num,last,first,middle;
+    int remainder = 0, reverse = 0;
+    cin >> x;
+    first = x;
+    num = x;
+    /*
+    int digits = (int)log10(x) + 1;
+
+    last = x%10;
+    while(first>10) first = first / 10;
+    middle = (int)(x / pow(10, digits / 2)) % 10;
+    */ //BASIC CONVERSIONS
+
+
+    //reverse
+    while(x != 0){
+        remainder = x%10;
+        reverse = reverse*10 + remainder;
+        x /= 10;
+    }
+
+    if(num == reverse) return true;
+    else return false;
+
+    return 0;
+}
