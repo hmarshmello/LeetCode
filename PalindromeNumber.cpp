3e261ae925ef3c48fc494bd6e3ea7d2700ab7d0f
Harsh Shrivastava <62792682+hmarshmello@users.noreply.github.com>
2022-09-24 11:00:06 +0530

    Updates

--- PalindromeNumber.cpp
+++ PalindromeNumber.cpp
@@ -2,18 +2,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    int x,num,last,first,middle;
+    int x,num;
     int remainder = 0, reverse = 0;
     cin >> x;
-    first = x;
     num = x;
-    /*
-    int digits = (int)log10(x) + 1;
-
-    last = x%10;
-    while(first>10) first = first / 10;
-    middle = (int)(x / pow(10, digits / 2)) % 10;
-    */ //BASIC CONVERSIONS
 
 
     //reverse
@@ -27,4 +19,14 @@ int main(){
     else return false;
 
     return 0;
+
+    /* //BASIC CONVERSIONS
+    int last, first, middle;
+    first = x;
+    int digits = (int)log10(x) + 1;
+
+    last = x%10;
+    while(first>10) first = first / 10;
+    middle = (int)(x / pow(10, digits / 2)) % 10;
+    */ //BASIC CONVERSIONS
 }
